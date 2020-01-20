a046046e269ceb50080a0d0255811ac9276ec5d5
Max Mueggler <maximumsomething@gmail.com>
2020-01-20 16:58:19 +0000

    Forward stream over ssh

--- compile_pi.sh
+++ compile_pi.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [ -z "$PI_ADDR" ]; then PI_ADDR=team5708pi.local; fi
+if [ -z "$PORT" ]; then PORT=22; fi
 
-rsync -rc -e "ssh -p 22" `dirname $0`"/src" "pi@"$PI_ADDR":./vision-code/"
-ssh pi@$PI_ADDR -p 22 "cd ~/vision-code/src && make build -j4 && make install"
+rsync -rc -e "ssh -p $PORT" `dirname $0`"/src" "pi@"$PI_ADDR":./vision-code/"
+ssh pi@$PI_ADDR -p $PORT "cd ~/vision-code/src && make build -j4 && make install"
