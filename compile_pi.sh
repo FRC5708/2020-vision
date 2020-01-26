3c889e047b8e64588f82eafa176918647c0d363b
Andrew Plotner <bluebandit21@gmail.com>
2020-01-26 15:22:29 -0500

    dafs

--- compile_pi.sh
+++ compile_pi.sh
@@ -2,5 +2,5 @@
 if [ -z "$PI_ADDR" ]; then PI_ADDR=team5708pi.local; fi
 if [ -z "$PORT" ]; then PORT=22; fi
 
-rsync -rc -e "ssh -p $PORT" `dirname $0`"/src" "pi@"$PI_ADDR":./vision-code/"
-ssh pi@$PI_ADDR -p $PORT "cd ~/vision-code/src && make build -j4 && make install"
+rsync -rc -e "ssh -p $PORT" `dirname $0`"/src" "pi@"$PI_ADDR":./vision-code/$(git rev-parse --abbrev-ref HEAD)/"
+ssh pi@$PI_ADDR -p $PORT "cd ~/vision-code/$(git rev-parse --abbrev-ref HEAD)/src && make build -j4 && make install"
