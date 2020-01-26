5bc04c34d985c96f083c355edd774ef3928c071a
Max Mueggler <maximumsomething@gmail.com>
2020-01-26 22:08:06 +0000

    includes and seperate build folder

--- compile_pi.sh
+++ compile_pi.sh
@@ -2,5 +2,5 @@
 if [ -z "$PI_ADDR" ]; then PI_ADDR=team5708pi.local; fi
 if [ -z "$PORT" ]; then PORT=22; fi
 
-rsync -rc -e "ssh -p $PORT" `dirname $0`"/src" "pi@"$PI_ADDR":./vision-code/"
-ssh pi@$PI_ADDR -p $PORT "cd ~/vision-code/src && make build -j4 && make install"
+rsync -rc -e "ssh -p $PORT" `dirname $0`"/src" "pi@"$PI_ADDR":./vision-code-jpeg/"
+ssh pi@$PI_ADDR -p $PORT "cd ~/vision-code-jpeg/src && make build -j4 && make install"
