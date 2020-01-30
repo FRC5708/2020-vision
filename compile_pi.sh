5346e4a71de86b7dd965978852c119f26d24fbf4
Max Mueggler <maximumsomething@gmail.com>
2020-01-29 20:36:19 -0500

    made compile_pi suck less

--- compile_pi.sh
+++ compile_pi.sh
@@ -2,5 +2,12 @@
 if [ -z "$PI_ADDR" ]; then PI_ADDR=team5708pi.local; fi
 if [ -z "$PORT" ]; then PORT=22; fi
 
-rsync -rc -e "ssh -p $PORT" `dirname $0`"/src" "pi@"$PI_ADDR":./vision-code/$(git rev-parse --abbrev-ref HEAD)/"
-ssh pi@$PI_ADDR -p $PORT "cd ~/vision-code/$(git rev-parse --abbrev-ref HEAD)/src && make build -j4 && make install"
+set -e
+
+PI_DIR="vision-code/$(git rev-parse --abbrev-ref HEAD)"
+CODE_DIR=$(dirname $0)
+
+set -x
+
+rsync -rc -e "ssh -p $PORT" "$CODE_DIR/src" "pi@"$PI_ADDR":./$PI_DIR/"
+ssh pi@$PI_ADDR -p $PORT "cd ~/$PI_DIR/src && make build -j4 && make install"
